ec0c91a2b78896672b2276284fcd71ed7e9ab178
HydeCW <blimple10@gmail.com>
2018-01-13 15:13:19 -0700

    Added product grid and selection buttons
    Added a working product grid that changes based on what selection button you push.

--- src/vendingMachine/Main.java
+++ src/vendingMachine/Main.java
@@ -1,40 +1,112 @@
 package vendingMachine;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import vendingMachine.classes.Dispenser;
 import vendingMachine.classes.gui.ProductPane;
+import vendingMachine.classes.products.Drink;
 import vendingMachine.classes.products.Product;
+import vendingMachine.classes.products.Snack;
 
 public class Main extends Application{
 
     // Dispencer class creation
-    Dispenser dispenser = new Dispenser(20.00, 0);
+    private Dispenser dispenser = new Dispenser(20.00, 0);
+    private Insets padding = new Insets(5,5,5,5);
+    private String borderedItems = "-fx-border-color: blue;\n"
+            + "-fx-border-insets: 6;\n"
+            + "-fx-border-width: 2;\n"
+            + "-fx-border-style: solid;\n";
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        GridPane gridPane = new GridPane();
+        //Create and Fromat the mainPane
+        BorderPane mainPane = new BorderPane();
+        mainPane.setPadding(padding);
+        mainPane.setPrefSize(300,300);
 
-        int counter = 0;
+        //Create pane that will hold all center content
+        HBox mainHBox = new HBox();
+        mainHBox.setPadding(padding);
 
-        for(int i =0; i < 4; i++){
-            for(int j = 0; j< 6; j++){
-                gridPane.add(new ProductPane(dispenser.getProductList().get(counter)), j, i);
-                counter++;
-            }
-        }
+        //Create hbox to hold product selection buttons
+        HBox productSelectionHBox = new HBox();
+        productSelectionHBox.setPadding(padding);
+
+        // vbox to hold grid
+        VBox productVBox = new VBox();
+        productVBox.setPadding(padding);
+
+        //vbox to hold cart grid , total price cost, and confirmation button
+        VBox cartVBox = new VBox();
+        cartVBox.setPadding(padding);
+
+        //Grid pane for the products
+        GridPane productGrid = new GridPane();
 
 
+        //Button for selecting Drink
+        Button btnDrink = new Button("Drinks");
+        btnDrink.setOnAction(event -> {
+           buildProductGrid(productGrid, true);
+        });
 
-        Scene scene = new Scene(gridPane);
+        //Button for selecting Snack
+        Button btnSnack = new Button("Snacks");
+        btnSnack.setOnAction(event -> {
+            buildProductGrid(productGrid, false);
+        });
+
+        //Add Nodes to panes
+        mainPane.setCenter(mainHBox);
+        mainHBox.getChildren().addAll(productVBox, cartVBox);
+        productSelectionHBox.getChildren().addAll(btnDrink, btnSnack);
+        productVBox.getChildren().addAll(productSelectionHBox, productGrid);
+
+        //Set Scene
+        Scene scene = new Scene(mainPane, 800, 600);
+
+        //Set primaryStage
         primaryStage.setTitle("Vending Machine");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    public void buildProductGrid( GridPane gridPane, boolean isDrink){
+        // Clear gridPane's previous content
+        gridPane.getChildren().clear();
+
+        // counter for indexing products
+        int counter = 0;
+
+        // imbeded for loop for filling gridPane
+        for(int i =0; i < 4; i++){
+            for(int j = 0; j< 6; j++){
+                //See if the product is a Drink or not and populate gridPane accordingly
+                if(isDrink){
+                    if(dispenser.getProductList().get(counter) instanceof Drink) {
+                        gridPane.add(new ProductPane(dispenser.getProductList().get(counter)), j, i);
+                    }
+                }else {
+                    if(dispenser.getProductList().get(counter) instanceof Snack) {
+                        gridPane.add(new ProductPane(dispenser.getProductList().get(counter)), j, i);
+                    }
+                }
+                //Incrament counter
+                counter++;
+            }
+        }
+    }
+
 
     public static void Main(String[] args){
         launch(args);
--- src/vendingMachine/classes/gui/ProductPane.java
+++ src/vendingMachine/classes/gui/ProductPane.java
@@ -1,16 +1,22 @@
 package vendingMachine.classes.gui;
 
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import vendingMachine.classes.products.Product;
 
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 
-public class ProductPane extends StackPane {
+public class ProductPane extends VBox {
 
+    private String labelStyle = "-fx-background-color: black; -fx-text-fill: white; -fx-label-padding: 5; -fx-font: Calibri;";
+    private ArrayList<Product> cartList;
 
     //basic constructor
     public ProductPane(){}
@@ -23,23 +29,39 @@ public class ProductPane extends StackPane {
 
             // Create an ImageView of the product image
             ImageView productImage = new ImageView(new Image("res/images/" + product.getImageName()));
-            productImage.setFitWidth(200);
-            productImage.setFitHeight(200);
+            productImage.setFitWidth(100);
+            productImage.setFitHeight(100);
+
+
+
+            // Label for displaying the name of the product
+            Label productName = new Label(product.getProductName());
+            productName.setWrapText(true);
+            productName.setStyle(labelStyle);
+
             // Label for displaying the price of the product
-            Label productPrice = new Label("$" + product.getPrice());
+            Label productPrice = new Label("$" +String.valueOf(product.getPrice()) + "Stock: " +String.valueOf(product.getQuantity()));
             productPrice.setWrapText(true);
-            productPrice.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-label-padding: 5; -fx-font: Calibri");
+            productPrice.setStyle(labelStyle);
+
 
             //TODO: edit the to string method to better detail the product
-            /* Label for displaying the detials of the product
             Label productDetails = new Label(product.toString());
             productDetails.setWrapText(true);
-            */
+
             //Add nodes to productPane
-            this.getChildren().addAll(productImage, productPrice);
+            this.getChildren().addAll(productImage, productName, productPrice);
 
             //TODO: Fromat StackPane to look good.
-        this.setStyle("-fx-alignment: bottom-center");
+
+        this.setAlignment(Pos.CENTER);
     }
 
+    public ArrayList<Product> getCartList() {
+        return cartList;
+    }
+
+    public void setCartList(ArrayList<Product> cartList) {
+        this.cartList = cartList;
+    }
 }
