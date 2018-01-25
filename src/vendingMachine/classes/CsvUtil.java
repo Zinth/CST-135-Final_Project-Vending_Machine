e0aa61aaba0badea5140eb319166ec0a265551ee
James Ray <james@rayprogramming.com>
2018-01-25 00:03:11 -0500

    Updated CSV Util to be able to read

--- src/vendingMachine/classes/CsvUtil.java
+++ src/vendingMachine/classes/CsvUtil.java
@@ -11,11 +11,17 @@
  */
 package vendingMachine.classes;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CsvUtil {
     private String path;
@@ -28,17 +34,53 @@ public class CsvUtil {
         fileWriter = new FileWriter(file);
     }
     
-    public void writeLine(List<String> values) throws IOException{
-        StringBuilder sb = new StringBuilder();
-        for (String value : values) {
-            sb.append('"').append(value).append('"').append(',');
+    public void writeLine(List<String> values){
+        try {
+            StringBuilder sb = new StringBuilder();
+            for (String value : values) {
+                sb.append('"').append(value).append('"').append(',');
+            }
+            sb.append("\n");
+            fileWriter.append(sb.toString());
+        } catch (IOException ex) {
+            Logger.getLogger(CsvUtil.class.getName()).log(Level.SEVERE, null, ex);
         }
-        sb.append("\n");
-        fileWriter.append(sb.toString());
     }
-    public void writeLines(List<List<String>> lines) throws IOException{
+    public void writeLines(List<List<String>> lines){
         for(List<String> line : lines){
             writeLine(line);
         }
+    }
+    
+    public HashMap<Integer, String[]> readCSV(){
+        BufferedReader br = null;
+        String line;
+        HashMap<Integer, String[]> linesAndValues = new HashMap<>();
+
+        try {
+
+            br = new BufferedReader(new FileReader(file));
+            int lineNumber = 1;
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                
+                linesAndValues.put(lineNumber, line.split(","));
+                lineNumber++;
+            }
+
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(CsvUtil.class.getName()).log(Level.SEVERE, null, e);
+        } catch (IOException e) {
+            Logger.getLogger(CsvUtil.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    Logger.getLogger(CsvUtil.class.getName()).log(Level.SEVERE, null, e);
+                }
+            }
+        }
+        return linesAndValues;
     }
 }
